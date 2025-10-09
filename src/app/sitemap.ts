442070574177ba4a1128fcd65e36ce9479bfd366
glpzghoo <my.as.glpzghoo@gmail.com>
2025-10-09 21:33:15 +0800

    correct

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://genshin-gw.com/';
+  const baseUrl = 'https://genshin-gw.com';
 
   // Static pages like home, about, etc.
   const staticPages = [{ url: baseUrl, lastModified: new Date() }];
